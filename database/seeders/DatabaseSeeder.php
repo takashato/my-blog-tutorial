99a9ea04c3699e9826cc81ce05ea258c4ac6ce66
trikhoi <trikhoi1670@gmail.com>
2022-08-05 20:31:23 +0700

    create sample user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -20,5 +22,11 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        User::factory()->create([
+            'name' => 'Admin',
+            'email' => 'admin@test.com',
+            'password' => Hash::make('123456test'),
+        ]);
     }
 }
